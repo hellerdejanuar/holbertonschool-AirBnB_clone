6134f9f06927ace0e0315d2d6ba430df6d9bce76
hellerdejanuar <hellerdejanuar@gmail.com>
2022-06-28 14:59:36 -0700

    not working part 4

--- models/base_model.py
+++ models/base_model.py
@@ -8,11 +8,18 @@ import uuid
 class BaseModel():
     """ Base Class """
 
-    def __init__(self):
-        """ Initializator """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+    def __init__(self, *args, **kwargs):
+        if kwargs:
+            for key in kwargs:
+                if key != '__class__':
+                    if (key == 'updated_at' or key == 'created_at'):
+                        setattr(self, key, datetime.fromisoformat(kwargs[key])
+                    else:
+                    	setattr(self, key, kwargs[key])
+        else:
+        	self.id = str(uuid.uuid4())
+        	self.created_at = datetime.now()
+        	self.updated_at = datetime.now()
 
     def __str__(self):
         """ Print string method """
