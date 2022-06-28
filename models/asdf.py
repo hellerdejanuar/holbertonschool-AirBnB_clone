72e86a8f47e88197b319b833996ecda51a2cb29f
hellerdejanuar <hellerdejanuar@gmail.com>
2022-06-28 14:05:13 -0700

    all working

--- models/asdf.py
+++ models/asdf.py
@@ -11,8 +11,8 @@ class BaseModel():
     def __init__(self):
         """ Initializator """
         self.id = str(uuid.uuid4()) 
-        self.created_at: datetime.now()
-        self.updated_at: datetime.now()
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def __str__(self):
         """ Print string method """
@@ -24,5 +24,5 @@ class BaseModel():
 
     def to_dict(self):
         """ Returns a key/value dictionary """
-        return {'id': self.id, 'created_at': self.created_at.isoformat("%Y-%m-%dT%H:%M:%S.%f"),
-                'updated_at': self.updated_at, '__class__': self.__class__.__name__}
+        return {'id': self.id, 'created_at': self.created_at.isoformat('T', 'auto'),
+                'updated_at': self.updated_at.isoformat('T', 'auto'), '__class__': self.__class__.__name__}
