1098a73d427905fab4d0ee63695a3af0a8a266d1
FloLys <4440@holbertonschool.com>
2022-06-28 14:11:30 -0700

    update

--- models/base_model.py
+++ models/base_model.py
@@ -1,22 +1,30 @@
 #!/usr/bin/python3
-""" base model """
+""" Base Model """
+import json
+from datetime import datetime
+import uuid
 
-class BaseModel:
-    
+
+class BaseModel():
+    """ Base Class """
+
     def __init__(self):
-        id = #assign with an uuid CASTED TO STR when created (unique uid for each)
-        created_at =
-        updated_at =
+        """ Initializator """
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def __str__(self):
-        classname, self id, self __dict__
+        """ Print string method """
+        return (f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}")
 
     def save(self):
-        updated_at = now()
+        """ Save changes and update updated_at """
+        self.updated_at = datetime.now()
 
     def to_dict(self):
-    #???? return the __dict with all the keys + __class__ key with the name of the class + updated_at and created_at must be co0nverted to string using ISO format
-
-        return self.__dict__.update({'__clas__': self.__class__}) 
-
-
+        """ Returns a key/value dictionary """
+        return {'id': self.id,
+                'created_at': self.created_at.isoformat('T', 'auto'),
+                'updated_at': self.updated_at.isoformat('T', 'auto'),
+                '__class__': self.__class__.__name__}
