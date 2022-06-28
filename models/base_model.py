5f4f0a2d84a96bc569c6d21be6e2096246b75374
hellerdejanuar <hellerdejanuar@gmail.com>
2022-06-28 10:08:23 -0700

    base

---
+++ models/base_model.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" base model """
+
+class BaseModel:
+    
+    def __init__(self):
+        id = #assign with an uuid CASTED TO STR when created (unique uid for each)
+        created_at =
+        updated_at =
+
+    def __str__(self):
+        classname, self id, self __dict__
+
+    def save(self):
+
+
