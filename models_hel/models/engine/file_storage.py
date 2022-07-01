45ed07a35eca50703ab2f42081bcb651abb47c3e
hellerdejanuar <hellerdejanuar@gmail.com>
2022-07-01 08:24:49 -0700

    heller something something

--- models_hel/models/engine/file_storage.py
+++ models_hel/models/engine/file_storage.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ Engine Module """
 import json
+from models.base_model import BaseModel
 
 
 class FileStorage():
@@ -39,8 +40,7 @@ class FileStorage():
     @staticmethod
     def classes():
         """ Returns a dict of classes """
-        from models.base_model import BaseModel
-        list_classes = {
+        classes_dict = {
             "BaseModel": BaseModel,
         }
-        return list_classes
+        return classes_dict
