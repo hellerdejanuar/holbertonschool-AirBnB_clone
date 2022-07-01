545ce3d9556aee17b303b59fb7f9b0a1e4b70288
FloLys <4440@holbertonschool.com>
2022-07-01 10:11:18 -0700

    update classes on filestorage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -32,10 +32,23 @@ class FileStorage():
 
     def reload(self):
         """ Deserializes the JSON file to __objects """
-        from models.user import User
         from models.base_model import BaseModel
+        from models.user import User
+        from models.state import State
+        from models.city import City
+        from models.amenity import Amenity
+        from models.place import Place
+        from models.review import Review
         
-        classes = {'BaseModel': BaseModel, 'User': User}
+        classes = {
+            'BaseModel': BaseModel,
+            'User': User,
+            'State': State,
+            'City': City,
+            'Amenity': Amenity,
+            'Place': Place,
+            'Review': Review
+        }
 
         try:
             with open(self.__file_path, 'r') as f:
