4f8abc5b4fd8cd7249131da2f9c97750bf653a98
FloLys <4440@holbertonschool.com>
2022-06-30 17:17:31 -0700

    upload user

---
+++ models/user.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" User Base Module """
+from models.base_model import BaseModel
+
+class User(BaseModel):
+    """ Class User """
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
