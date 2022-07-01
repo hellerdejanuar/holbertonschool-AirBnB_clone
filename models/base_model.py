28814a80dc8cb4fd358aca011665d9465af00628
FloLys <4440@holbertonschool.com>
2022-06-30 19:07:17 -0700

    UPPDATE 5 100% WORKING WARNING NO CHANGE

--- models/base_model.py
+++ models/base_model.py
@@ -38,9 +38,9 @@ class BaseModel():
 
     def to_dict(self):
         """ Returns a key/value dictionary """
-        #return_dict = self.__dict__
-        self.__dict__["created_at"] = self.created_at.isoformat()
-        self.__dict__["updated_at"] = self.updated_at.isoformat()
-        self.__dict__["__class__"] = self.__class__.__name__
-        #return_dict.update({'__class__' : self.__class__.__name__})
-        return self.__dict__
+        new_dict = self.__dict__.copy()
+        new_dict["created_at"] = self.created_at.isoformat()
+        new_dict["updated_at"] = self.updated_at.isoformat()
+        new_dict["__class__"] = self.__class__.__name__
+        #new_dict.update({'__class__' : self.__class__.__name__})
+        return new_dict
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -18,20 +18,30 @@ class FileStorage():
 
     def new(self, obj):
         """ Sets in __objects the obj with key <obj class name>.id"""
-        self.__objects.update({f"{obj.__class__.__name__}.{obj.id}" : obj.to_dict()})
+        self.__objects.update({obj.to_dict()['__class__'] + '.' + obj.id: obj})
+        #self.__objects.update({f"{obj.__class__.__name__}.{obj.id}" : obj.to_dict()})
 
     def save(self):
         """ Serializes __objects to the JSON file """
-        if self.__file_path:
-            with open(self.__file_path, 'w') as f:
-                f.write(json.dumps(self.__objects))
-        else:
-            self.__objects = {}
+        with open(self.__file_path, 'w') as f:
+            for key, value in self.__objects.items():
+                self.__objects[key] = value.to_dict()
+            json.dump(self.__objects, f)
 
     def reload(self):
         """ Deserializes the JSON file to __objects """
+        from models.base_model import BaseModel
+        from models.user import User
+        classes = {'BaseModel': BaseModel, 'User': User}
         try:
             with open(self.__file_path, 'r') as f:
-                {self.__objects.update(json.loads(dic)) for dic in f}
+                self.__objects = json.loads(f.read())
+                for key, value in self.__objects.items():
+                    self.__objects[key] = classes[value['__class__']](**value)
         except FileNotFoundError:
-            return
+            pass
+        #try:
+        #    with open(self.__file_path, 'r') as f:
+        #        {self.__objects.update(json.loads(dic)) for dic in f.read()}
+        #except FileNotFoundError:
+        #    return
