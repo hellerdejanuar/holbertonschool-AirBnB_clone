4eeb870727319a6f12e7d7ba04a81fbb4c8f9afd
hellerdejanuar <hellerdejanuar@gmail.com>
2022-07-02 13:19:24 -0700

    advancements in storing list of strings in amenity_ids

--- console.py
+++ console.py
@@ -109,13 +109,9 @@ class HBNBCommand(cmd.Cmd):
                     elif attr_type == float:
                         setattr(obj, argv[2], float(argv[3]))
                     elif attr_type == list:
-                        print(argv[3])
                         str_list = argv[3].strip('][').split(', ')
-                        #str_list = [str(i) for i in json.loads(argv[3])]
-                        print(str_list)
                         setattr(obj, argv[2], str_list)
                         # convert string repr of list "[1, 2, 3]" into list obj
-                        # TEST pending ////
                     elif attr_type == str:
                         setattr(obj, argv[2], argv[3])
                     else:
