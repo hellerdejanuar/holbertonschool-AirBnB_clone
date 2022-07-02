ce7dd46ad1a33185b9e92975e1b902cd29f20d50
hellerdejanuar <hellerdejanuar@gmail.com>
2022-07-02 10:32:21 -0700

    upd

---
+++ tests/test_console.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Tests for the command interpreter
+"""
+import unittest
+from console import HBNBCommand
+
+
+class TestConsole(unittest.TestCase):
+
+    def test_prompt(self):
+        """test for the correct prompt"""
+        self.assertEqual('(hbnb) ', HBNBCommand.prompt)
+
+    def test_quit(self):
+        """test when receiving 'quit' command"""
+        pass
+
+    #def test_output(self):
+    #    """
