ef864ad0b0cc1590d78db15b771b51fed19e95d6
FloLys <4440@holbertonschool.com>
2022-07-03 10:37:23 -0700

    pycodestyle update

--- tests/test_console.py
+++ tests/test_console.py
@@ -18,36 +18,49 @@ from unittest.mock import patch
 from io import StringIO
 import os
 
+
 class TestConsole(unittest.TestCase):
 
     def test_prompt(self):
-        """test for the correct prompt"""
+        """
+        test for the correct prompt
+        """
         self.assertEqual('(hbnb) ', HBNBCommand.prompt)
 
     def test_quit(self):
-        """test when receiving 'quit' command"""
+        """
+        test when receiving 'quit' command
+        """
         pass
 
     def test_do_quit(self):
-        """Test do_quit method"""
+        """
+        Test do_quit method
+        """
         with patch('sys.stdout', new=StringIO()) as input_t:
             HBNBCommand().onecmd("quit")
             self.assertEqual(input_t.getvalue(), "")
 
     def test_EOF(self):
-        """Test do_EOF method"""
+        """
+        Test do_EOF method
+        """
         with patch('sys.stdout', new=StringIO()) as input_t:
             HBNBCommand().onecmd("EOF")
             self.assertEqual(input_t.getvalue(), "")
 
     def test_emptyline(self):
-        """test emptyline method"""
+        """
+        test emptyline method
+        """
         with patch('sys.stdout', new=StringIO()) as input_t:
             HBNBCommand().onecmd("\n")
             self.assertEqual(input_t.getvalue(), "")
 
     def test_do_create(self):
-        """test do_create method"""
+        """
+        test do_create method
+        """
         with patch('sys.stdout', new=StringIO()) as input_t:
             HBNBCommand().onecmd("create")
             self.assertEqual(input_t.getvalue(), "** class name missing **\n")
@@ -56,7 +69,9 @@ class TestConsole(unittest.TestCase):
             self.assertEqual(input_t.getvalue(), "** class doesn't exist **\n")
 
     def test_do_show(self):
-        """test do_show method"""
+        """
+        test do_show method
+        """
         with patch('sys.stdout', new=StringIO()) as input_t:
             HBNBCommand().onecmd("show")
             self.assertEqual(input_t.getvalue(), "** class name missing **\n")
@@ -71,7 +86,9 @@ class TestConsole(unittest.TestCase):
             self.assertEqual(input_t.getvalue(), "** no instance found **\n")
 
     def test_do_destroy(self):
-        """test do_destroy"""
+        """
+        test do_destroy
+        """
         with patch('sys.stdout', new=StringIO()) as input_t:
             HBNBCommand().onecmd("destroy")
             self.assertEqual(input_t.getvalue(), "** class name missing **\n")
@@ -86,13 +103,17 @@ class TestConsole(unittest.TestCase):
             self.assertEqual(input_t.getvalue(), "** no instance found **\n")
 
     def test_do_all(self):
-        """test do_all method"""
+        """
+        test do_all method
+        """
         with patch('sys.stdout', new=StringIO()) as input_t:
             HBNBCommand().onecmd("all Userfdsf")
             self.assertEqual(input_t.getvalue(), "** class doesn't exist **\n")
 
     def test_do_update(self):
-        """test de_update method"""
+        """
+        test de_update method
+        """
         with patch('sys.stdout', new=StringIO()) as input_t:
             HBNBCommand().onecmd("update")
             self.assertEqual(input_t.getvalue(), "** class name missing **\n")
@@ -105,4 +126,3 @@ class TestConsole(unittest.TestCase):
         with patch('sys.stdout', new=StringIO()) as inpt:
             HBNBCommand().onecmd("update User 9")
             self.assertEqual(inpt.getvalue(), "** attribute name missing **\n")
-
